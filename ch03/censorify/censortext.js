2faff06df4ead115359156c561b76be8461547e8
luhuadong <luhuadong@163.com>
2018-01-24 12:04:03 +0800

    Update.

--- ch03/censorify/censortext.js
+++ ch03/censorify/censortext.js
@@ -5,7 +5,6 @@ function censor(inStr) {
 	for (idx in censoredWords) {
 		inStr = inStr.replace(censoredWords[idx], "****");
 	}
-
 	for (idx in customCensoredWords) {
 		inStr = inStr.replace(customCensoredWords[idx], "****");
 	}
